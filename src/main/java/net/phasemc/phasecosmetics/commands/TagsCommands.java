78e092bb0890ce3640dd4655aa51c36e6ca4b1dd
Blueil <49593489+Blueil@users.noreply.github.com>
2021-04-30 09:54:12 +0300

    Update TagsCommands.java

--- src/main/java/net/phasemc/phasecosmetics/commands/TagsCommands.java
+++ src/main/java/net/phasemc/phasecosmetics/commands/TagsCommands.java
@@ -20,7 +20,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
