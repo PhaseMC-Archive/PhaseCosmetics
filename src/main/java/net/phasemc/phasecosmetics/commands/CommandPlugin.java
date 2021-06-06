f8914d0596fbe29872e783ed2c0282a1aba52bd1
Jordan Adams <JordanPlayz158again@gmail.com>
2021-06-06 17:49:57 -0400

    Added reload command functionality

--- src/main/java/net/phasemc/phasecosmetics/commands/CommandPlugin.java
+++ src/main/java/net/phasemc/phasecosmetics/commands/CommandPlugin.java
@@ -1,5 +1,7 @@
 package net.phasemc.phasecosmetics.commands;
 
+import net.phasemc.phasecosmetics.PhaseCosmetics;
+import net.phasemc.phasecosmetics.gui.GuiParser;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,20 +13,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class CommandPlugin implements CommandExecutor, TabCompleter {
-
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length == 0) {
             sender.sendMessage(ChatColor.RED + "Too few arguments!");
             return false;
         }
-        switch (args[0].toLowerCase()) {
 
-            default:
-                sender.sendMessage(ChatColor.RED + "No " + args[0] + " subcommand.");
+        switch (args[0].toLowerCase()) {
             case "reload":
                 sender.sendMessage(ChatColor.GREEN + "PhaseCosmetics config reloaded!");
 
+                try {
+                    PhaseCosmetics.plugin.reloadConfig();
+
+                    //System.out.println();
+
+                    new GuiParser(PhaseCosmetics.plugin.getConfig()).makeInventory();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    sender.sendMessage(ChatColor.RED + "PhaseCosmetics GUI -> " + e.getMessage());
+                }
+
+                break;
+            default:
+                sender.sendMessage(ChatColor.RED + "No " + args[0] + " subcommand.");
+                break;
         }
         return true;
     }
